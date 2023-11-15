7a5b8bae5a7ba4e818be5a4b662c21d62cc98d88
SOOS-JAlvarez <jalvarez@soos.io>
2023-11-15 09:49:46 -0300

    PA-11741 SBOM Github Action

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+# dockerifle for node
+FROM node:16-slim as BASE
+
+RUN npm i -g @soos-io/soos-sbom
+
+COPY entrypoint.sh /entrypoint.sh
+
+RUN chmod +x /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
+CMD ["-h"]
---
+++ entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+
+SOOS_APP_VERSION=${GITHUB_ACTION_REF}
+
+SOOS_INTEGRATION_NAME="GitHub"
+SOOS_INTEGRATION_TYPE="Plugin"
+
+SOOS_BRANCH_NAME=${SOOS_BRANCH_NAME:-${GITHUB_REF}}
+SOOS_SBOM_PATH=${SOOS_SBOM_PATH:-${GITHUB_WORKSPACE}}
+
+PARAMS=(
+    "--apiKey ${SOOS_API_KEY}"
+    "--apiURL ${SOOS_API_URL}"
+    "--appVersion ${SOOS_APP_VERSION}"
+    "--branchName ${SOOS_BRANCH_NAME}"
+    ${SOOS_BRANCH_URI:+--branchUri ${SOOS_BRANCH_URI}}
+    ${SOOS_BUILD_URI:+--buildUri ${SOOS_BUILD_URI}}
+    ${SOOS_BUILD_VERSION:+--buildVersion ${SOOS_BUILD_VERSION}}
+    "--clientId ${SOOS_CLIENT_ID}"
+    "--commitHash ${GITHUB_SHA}"
+    "--integrationName ${SOOS_INTEGRATION_NAME}"
+    "--integrationType ${SOOS_INTEGRATION_TYPE}"
+    "--projectName ${SOOS_PROJECT_NAME}"
+    ${SOOS_VERBOSE:+--verbose}
+)
+
+PARAMS_STRING="${PARAMS[@]}"
+
+node soos-sbom "${SOOS_SBOM_PATH}" ${PARAMS_STRING}
